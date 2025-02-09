ebc8a090f213e8121b40c11bc3f7a0c141095183
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-02-09 16:26:55 -0300

    feat: implementa validacao de campos

--- src/app/auth/user-register-dialog/user-register-dialog.component.ts
+++ src/app/auth/user-register-dialog/user-register-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register-dialog',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './user-register-dialog.component.scss'
 })
 export class UserRegisterDialogComponent {
+  userForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.userForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      role: ['User', Validators.required]
+    },
+      { Validators: this.passwordMatchValidator }
+    )
+  }
+
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
 }
