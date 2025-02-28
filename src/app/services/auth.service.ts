6795370858725a35cb116ea12b0fd4db3fb1c049
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-02-28 17:16:14 -0300

    feat: adiciona guardiao de rotas para usuario autenticado e validacao de token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,10 +8,12 @@ import { Login } from '../shared/models/login';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
   private baseURL = `${envinronment.apiURL}/account`;
   private http = inject(HttpClient)
   private router = inject(Router);
+  private readonly TOKEN_KEY = 'userToken';
 
   login(credentials: Login): Observable<string> {
     return this.http.post<{ token?: string }>(`${this.baseURL}/login`, credentials).pipe(
@@ -35,10 +37,40 @@ export class AuthService {
   }
 
   isAuthenticated(): boolean {
-    return !!localStorage.getItem('userToken');
+    const token = localStorage.getItem(this.TOKEN_KEY);
+    if (!token) {
+      return false; //Sem token, usuário não autenticado
+    }
+
+    const tokenData = this.decodeToken(token);
+    if (!tokenData || !tokenData.exp) {
+      return false; // Token inválido
+    }
+
+    const expirationTime = tokenData.exp * 1000; // Convertendo para milissegundos
+    const now = Date.now();
+
+    if (expirationTime < now) {
+      this.logout(); // Remove o token se expirado
+      return false; // Token expirado
+    }
+
+    return true;// Usuário autenticado
   }
 
   private saveToken(token: string): void {
     localStorage.setItem('userToken', token);
+  }
+
+  private decodeToken(token: string): any {
+    try {
+      const tokenParts = token.split('.');
+      if (tokenParts.length !== 3) return null;
+
+      const payload = atob(tokenParts[1]); // Decodifica o payload do JWT
+      return JSON.parse(payload);
+    } catch (e) {
+      return null; // Retorna null se houver erro na decodificação
+    }
   }
 }
