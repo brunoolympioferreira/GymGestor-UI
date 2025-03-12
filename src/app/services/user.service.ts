af3caaa2cae070e71330095fa27b92a009f48c31
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-03-12 19:24:46 -0300

    feat: implementa servico de buscar usuario autenticado

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { envinronment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable, take } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,24 @@ import { Observable, take } from 'rxjs';
 export class UserService {
   private baseURL = `${envinronment.apiURL}/users`;
   private http = inject(HttpClient);
-  constructor() { }
+  private jwtHelper = inject(JwtHelperService);
 
   register(user: User): Observable<User> {
     return this.http.post<User>(`${this.baseURL}`, user).pipe(take(1));
+  }
+
+  getAuthenticatedUser(): Observable<User> {
+    const userId = this.getUserIdFromToken();
+    if (!userId) throw new Error('Usuário não autenticado');
+
+    return this.http.get<User>(`${this.baseURL}/${userId}`);
+  }
+
+  private getUserIdFromToken(): string | null {
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+
+    const decodedToken = this.jwtHelper.decodeToken(token);
+    return decodedToken?.user_id || null;
   }
 }
