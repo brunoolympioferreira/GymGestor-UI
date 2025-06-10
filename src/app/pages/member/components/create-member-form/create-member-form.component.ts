40b4f8ec5354b9ab877ad4dc4dc93987c2a2b309
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-06-10 14:11:43 -0300

    finaliza cadastro de membro

--- src/app/pages/member/components/create-member-form/create-member-form.component.ts
+++ src/app/pages/member/components/create-member-form/create-member-form.component.ts
@@ -14,6 +14,11 @@ import { DateAdapter } from '@angular/material/core';
 export class CreateMemberFormComponent {
   form: FormGroup;
   isSubmitting = false;
+  estados: string[] = [
+    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA',
+    'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN',
+    'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+  ];
 
   fb = inject(FormBuilder);
   memberService = inject(MemberService);
